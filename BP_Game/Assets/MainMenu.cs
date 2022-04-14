581fb6986cac3182618632362f819aea24848f95
Nyusko <103515975+Nyusko@users.noreply.github.com>
2022-04-15 01:11:08 +0200

    5

---
+++ BP_Game/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void QuitingGame() {
+        Debug.Log("Hra sa vypina...");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
